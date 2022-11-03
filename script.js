224fe8b22f1e37d839578f57bdf83d040f808dec
Nelly <nelianogs@gmail.com>
2022-11-03 02:45:43 +0100

    My Todo App

---
+++ script.js
@@ -0,0 +1,39 @@
+const add_btn = document.getElementById("btn");
+add_btn.addEventListener("click", addTodo);
+
+
+const edit_btn = document.getElementById("Edit");
+edit_btn.addEventListener("click", editTodo);
+
+const editTodo = document.getElementById
+
+const delete_btn = document.getElementById("Delete");
+delete_btn.addEventListener("click", deleteTodo);
+
+
+
+
+
+const li = document.querySelector("#todo_input")
+
+add_btn.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+
+
+
+
+function showTodoOnTheScreen(todo) {
+    const listOfTodo = document.querySelector("#todo__list");
+    const li = document.createElement("li");
+    li.classList.add("todo__section--todoItem");
+
+
+
+
+    const inputField = document.querySelector("#todo__input");
+    const todoValue = inputField.value;
+    showTodoOnTheScreen(todoValue);
+    inputField.value = ""
+}
