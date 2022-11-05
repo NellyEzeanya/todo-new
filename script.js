fdf0d7164f442a073b1d62ae089734b1295d72a4
Nelly <nelianogs@gmail.com>
2022-11-05 14:19:19 +0100

    ggg

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const inputField = document.querySelector("#todo__input");
+const todoValue = inputField.value;
+
 const add_btn = document.getElementById("btn");
-add_btn.addEventListener("click", addTodo);
 
 
 const edit_btn = document.getElementById("Edit");
@@ -16,22 +18,18 @@ const li = document.querySelector("#todo_input")
 
 add_btn.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log(e);
+    showTodoOnTheScreen(todoValue);
+    inputField.value = ""
 })
-
-
-
-
 
 function showTodoOnTheScreen(todo) {
     const listOfTodo = document.querySelector("#todo__list");
     const li = document.createElement("li");
     li.classList.add("todo__section--todoItem");
+    console.log(li);
+    li.innerText = todo;
+    listOfTodo.append(li)
 
-
-
-
-    const inputField = document.querySelector("#todo__input");
-    const todoValue = inputField.value;
-    showTodoOnTheScreen(todoValue);
-    inputField.value = ""
 }
+
