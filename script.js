d9cf94ff9300ef83b9b01b376a42a64470e1c823
Nelly <nelianogs@gmail.com>
2022-11-05 19:34:34 +0100

    Changes 5th Nov

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ editTodo.addEventListener("click", editTodo);
 
 const delete_btn = document.getElementById("Delete");
 delete_btn.addEventListener("click", deleteTodo);
+const form = document.querySelectorAll(".form");
 
 
-const li = document.querySelector("#todo_input")
+const listItem = document.querySelector("todo_input")
 
-add_btn.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
     showTodoOnTheScreen(todoValue);
@@ -29,7 +30,34 @@ function showTodoOnTheScreen(todo) {
     li.classList.add("todo__section--todoItem");
     console.log(li);
     li.innerText = todo;
+    li.innerHTML+= `<button id="Edit" type="button" class="edit_btn">Edit</button>
+    <button id="Delete" type="button" class="delete_btn">Delete</button>`
+    //prepend is at the top, append is at the button
     listOfTodo.append(li)
 
 }
+
+//show the submition of my todo form, submit handler
+//function showSubmitHandler(e){
+  //  e.preventDefault()
+    //const todo = inputField.value;
+//}
+//ADD EVENT LISTENER
+//1. element that the event is happening to: addTodoForm
+//2. call addEventListener PaymentMethodChangeEvent. experts two arguments
+    //a. event you ar listening for: submit
+    //b. function that will control what happens if the event happens.  A function that
+
+addTodoForm.addEventListener("submit", showSubmitHandler);
+
+
+
+function editTodo(){
+    const editingTodo = editBtn.previousElementSibling
+    // let todoBeingEdited = 
+    const editTodo = inputField.value = editingTodo.innerText
+    inputField.value 
+}
+
+addTodoForm.addEventListener("submit", submitHandler)
 
