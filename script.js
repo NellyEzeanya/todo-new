709f5e942d806a238d7746b0159ffa230edd4115
Nelly <nelianogs@gmail.com>
2022-11-11 18:38:38 +0100

    I added a new UI and functionality

--- script.js
+++ script.js
@@ -1,63 +1,39 @@
-const inputField = document.querySelector("#todo__input");
-const todoValue = inputField.value;
+const form = document.getElementById("form");
+const todo__list = document.getElementById("todo__list");
 
-const add_btn = document.getElementById("btn");
+// function to delete todo
+function deleteTodo() {
+    // this - is a reference to the current object
+    this.event.target.parentElement.parentElement.remove();
+}
 
+// function to edit todo
+function editTodo() {
+    const parent = this.event.target.parentElement.parentElement;
+    const newValue = prompt("Enter New Value");
+    parent.querySelector("span").innerHTML = newValue;
+}
 
-const edit_btn = document.getElementById("Edit");
-edit_btn.addEventListener("click", editTodo);
-
-const editTodo = document.getElementById("edit_btn");
-editTodo.addEventListener("click", editTodo);
-
-const delete_btn = document.getElementById("Delete");
-delete_btn.addEventListener("click", deleteTodo);
-const form = document.querySelectorAll(".form");
-
-
-const listItem = document.querySelector("todo_input")
-
-form.addEventListener("submit", (e) => {
+// function to add new todo
+function addNewTodo(e) {
     e.preventDefault();
-    console.log(e);
-    showTodoOnTheScreen(todoValue);
-    inputField.value = ""
-})
+    const todoValue = e.target.elements.form_input.value
+    e.target.elements.form_input.value = "";
 
-function showTodoOnTheScreen(todo) {
-    const listOfTodo = document.querySelector("#todo__list");
-    const li = document.createElement("li");
-    li.classList.add("todo__section--todoItem");
-    console.log(li);
-    li.innerText = todo;
-    li.innerHTML+= `<button id="Edit" type="button" class="edit_btn">Edit</button>
-    <button id="Delete" type="button" class="delete_btn">Delete</button>`
-    //prepend is at the top, append is at the button
-    listOfTodo.append(li)
-
+    // create elements
+    // append newly created element to the ul
+    todo__list.innerHTML += `
+        <li>
+            <span>${todoValue}</span>
+            <div>
+            <button id="Edit" type="button" class="edit_btn" onclick="editTodo()">Edit</button>
+            <button id="Delete" type="button" class="delete_btn" onclick="deleteTodo()">Delete</button>
+            </div>
+        </li>
+    `;
 }
-
-//show the submition of my todo form, submit handler
-//function showSubmitHandler(e){
-  //  e.preventDefault()
-    //const todo = inputField.value;
-//}
-//ADD EVENT LISTENER
-//1. element that the event is happening to: addTodoForm
-//2. call addEventListener PaymentMethodChangeEvent. experts two arguments
-    //a. event you ar listening for: submit
-    //b. function that will control what happens if the event happens.  A function that
-
-addTodoForm.addEventListener("submit", showSubmitHandler);
+form.addEventListener("submit", addNewTodo);
 
 
 
-function editTodo(){
-    const editingTodo = editBtn.previousElementSibling
-    // let todoBeingEdited = 
-    const editTodo = inputField.value = editingTodo.innerText
-    inputField.value 
-}
-
-addTodoForm.addEventListener("submit", submitHandler)
 
